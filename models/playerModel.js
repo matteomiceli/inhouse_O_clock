abe169cdc1e3b94b97ba50afc330ba07744244ea
matteomiceli <omatteomiceli@gmail.com>
2021-03-26 02:42:28 -0700

    updated playermodel with player class
    creates player and handles pushing them to database

--- models/playerModel.js
+++ models/playerModel.js
@@ -1,14 +1,61 @@
+const db = require('../src/database');
+
 
 // takes in a req.body, parses the data and returns an object that can 
 // be inserted into the database
-function playerModel (req) {
-    console.log(req);
 
+class Player {
+    name;
+    alias;
+    posRatings;
+    wins;
+    #password = 'tf';
+    #auth = true;
+
+
+    constructor (req) {
+        console.log(req);
+        if (req.password === this.#password) {
+            this.name = req.name;
+            this.alias = req.alias;
+            this.posRatings = {
+                top: req.top,
+                jung: req.jung,
+                mid: req.mid,
+                adc: req.adc,
+                sup: req.sup
+            }
+            this.wins = 0;
+            return;
+        }
+        this.#auth = false;
+    }
+
+    getAuth() {
+        return this.#auth;
+    }
+
+    setName(req) {
+        if (req.password === this.#password) {
+            this.name = req.name;
+            this.#auth = true;
+        } 
+        this.#auth = false;
+    }
+
+    setTop(req) {
+        if (req.password === this.#password) {
+            this.posRatings.top == req.top;
+        } 
+        return false;
+    }
+
+    pushToDb () {
+        db.insert(this);
+    }
 }
 
-
-module.exports =  playerModel;
-
+module.exports =  Player;
 
 // {
 //     name: 'asd',
