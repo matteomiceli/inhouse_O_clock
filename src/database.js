a9c4950d7fb8f2289a9aaff04f4d415bf24ff33a
matteo <matteo@pop-os.localdomain>
2021-04-29 01:51:32 -0700

    starting the transition to mongoDB

--- src/database.js
+++ src/database.js
@@ -1,15 +1,26 @@
-var Datastore = require('nedb')
-  let db = new Datastore({ filename: './src/players.db' });
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const db = mongoose.connection;
+mongoose.connect(process.env.DB_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log('connected to DB...'))
+  .catch((err) => console.log(err));
+
+// make a player model and then replace playerModel function with mongoose schema
 
 
-  // load database
-  db.loadDatabase((err) => {
-    if (err) {
-        console.log(err);
-        return; 
-    }
-    console.log('Database successfully loaded')
-});
+// var Datastore = require('nedb')
+//   let db = new Datastore({ filename: './src/players.db' });
+
+
+//   // load database
+//   db.loadDatabase((err) => {
+//     if (err) {
+//         console.log(err);
+//         return; 
+//     }
+//     console.log('Database successfully loaded')
+// });
 
 
 module.exports = db;
