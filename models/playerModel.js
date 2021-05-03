fff86279a79251fd07fc4a428128032c62e5a7c9
matteo <matteo@pop-os.localdomain>
2021-05-03 02:34:06 -0700

    sync

--- models/playerModel.js
+++ models/playerModel.js
@@ -1,10 +1,7 @@
 const mongoose = require('../src/database');
 const { Schema } = mongoose;
  
-
-// takes in a req.body, parses the data and returns an object that can 
-// be inserted into the database
-
+// schema for player model
 const playerSchema = new Schema({
     name: String,
     alias: String,
@@ -19,68 +16,8 @@ const playerSchema = new Schema({
     losses: Number
 }, {collection: 'players'})
 
+// player model 
 const Player = mongoose.model('newPlayer', playerSchema);
 
 
 module.exports = Player;
-
-// class Player {
-//     name;
-//     alias;
-//     posRatings;
-//     wins;
-//     losses;
-//     #password = 'tf';
-//     #auth = true;
-
-
-//     constructor(req) {
-//         console.log(req);
-//         if (req.password === this.#password) {
-//             this.name = req.name;
-//             this.alias = req.alias;
-//             this.posRatings = {
-//                 top: req.top,
-//                 jung: req.jung,
-//                 mid: req.mid,
-//                 adc: req.adc,
-//                 sup: req.sup
-//             }
-//             this.wins = 0;
-//             this.losses = 0;
-//             return;
-//         }
-//         this.#auth = false;
-//     }
-
-//     getAuth() {
-//         return this.#auth;
-//     }
-
-//     pushToDb() {
-//         db.insert(this);
-//     }
-// }
-
-
-
-
-// gameData structure
-// {
-//     winningTeam: 'red',
-//     red: {
-//       top: { alias: 'Hiii1324', score: '50' },
-//       jung: { alias: 'Bougino', score: '89' },
-//       mid: { alias: '', score: '' },
-//       adc: { alias: '', score: '' },
-//       sup: { alias: '', score: '' }
-//     },
-//     blu: {
-//       top: { alias: '', score: '' },
-//       jung: { alias: '', score: '' },
-//       mid: { alias: '', score: '' },
-//       adc: { alias: '', score: '' },
-//       sup: { alias: '', score: '' }
-//     },
-//     probability: { red: '0.9608565972483071', blue: '0.039143402751692924' }
-//   }
--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.get("/", (req, res) => {
-    res.render('index')
+    res.render('index');
 });
 
 
