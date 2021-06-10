89d465cc5802adb4aa525ea91ab50e652bd8fb42
matteomiceli <omatteomiceli@gmail.com>
2021-06-09 21:09:32 -0700

    tailwinds extended styles and beginning to style index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+//   purge: {
+//     enabled: true,
+//     content: [
+//         './src/**/*.html',
+//         './src/views/*.ejs',
+//     ],
+// },
+  darkMode: false, // or 'media' or 'class',
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'league-bg': "url('/img/league_bg.jpg')"
+      }),
+      fontFamily: {
+        body: ['Roboto', 'sans-serif'],
+        heading: ['Anton', 'sans-serif']
+      },
+      colors: {
+        'int-red': '#b4200f',
+        'int-blue': '#2583ffff'
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundOpacity: ['active']
+    },
   },
   plugins: [],
 }
