5afe2def1ef53c752a0324842d0ccc2bc845d4aa
matteomiceli <matteo@pop-os.localdomain>
2021-06-11 12:32:07 -0700

    menu button to close in js

--- src/public/js/menu.js
+++ src/public/js/menu.js
@@ -2,6 +2,12 @@ const nav = $('nav');
 const menuBtn = $('.menu-icon');
 
 menuBtn.click(() => {
+    if (menuBtn.attr('src') === '/img/menu.svg') {
+        menuBtn.attr('src', '/img/close_white.svg')
+    } else {
+        menuBtn.attr('src', '/img/menu.svg')
+    }
+
     nav.toggle(100, 'linear', () => {
 
     })
