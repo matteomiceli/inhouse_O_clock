80a24f64067d654aa56fb47a5e787814f87264f1
Matteo Miceli <matteo@Matteos-MacBook-Air.local>
2021-07-24 16:30:21 -0700

    animated dropdown menu

--- src/public/js/menu.js
+++ src/public/js/menu.js
@@ -1,12 +1,15 @@
 const nav = $('nav');
 const menuBtn = $('.menu-icon');
+const header = $('header')
 
 menuBtn.click(() => {
     if (menuBtn.attr('src') === '/img/menu.svg') {
         menuBtn.attr('src', '/img/close_white.svg');
-        nav.css('display', 'flex');
+        nav.css('max-height', '1000px');
+        // header.addClass('shadow-xl')
     } else {
+        header.removeClass('shadow-xl')
         menuBtn.attr('src', '/img/menu.svg');
-        nav.css('display', 'none');
+        nav.css('max-height', '0px');        
     }
 })
