2ea75ae43cdaac4d87fdabc03a9bd6826c7f38f1
Matteo Miceli <omatteomiceli@gmail.com>
2022-03-06 09:00:03 -0800

    purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,39 @@
-const colors = require('tailwindcss/colors');
-const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-//   purge: {
-//     enabled: true,
-//     content: [
-//         './src/**/*.html',
-//         './src/views/*.ejs',
-//         './src/public/js/*.js',
-//     ],
-// },
-  darkMode: false, // or 'media' or 'class',
-  theme: {
-    extend: {
-      backgroundImage: theme => ({
-        'league-bg': "url('/img/league_bg.jpg')",
-        'league-banner': "url('/img/league-banner.jpg')",
-        'crown': "url('/img/logo_black.png')"
-      }),
-      fontFamily: {
-        body: ['Noto Sans JP', 'sans-serif'],
-        heading: ['Anton', 'sans-serif']
-      },
-      colors: {
-        'int-red': '#b4200f',
-        'int-red-light': '',
-        'int-blue': '#2583ffff',
-        'int-blue-light': '#59a1ff'
-      },
+    purge: {
+        enabled: true,
+        content: [
+            "./src/**/*.html",
+            "./src/views/*.ejs",
+            "./src/public/js/*.js",
+        ],
     },
-  },
-  variants: {
-    extend: {
-      backgroundOpacity: ['active']
+    darkMode: false, // or 'media' or 'class',
+    theme: {
+        extend: {
+            backgroundImage: (theme) => ({
+                "league-bg": "url('/img/league_bg.jpg')",
+                "league-banner": "url('/img/league-banner.jpg')",
+                crown: "url('/img/logo_black.png')",
+            }),
+            fontFamily: {
+                body: ["Noto Sans JP", "sans-serif"],
+                heading: ["Anton", "sans-serif"],
+            },
+            colors: {
+                "int-red": "#b4200f",
+                "int-red-light": "",
+                "int-blue": "#2583ffff",
+                "int-blue-light": "#59a1ff",
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    variants: {
+        extend: {
+            backgroundOpacity: ["active"],
+        },
+    },
+    plugins: [],
+};
